74bdf1ba380ba15a6faae59e46a9243a9ae235eb
Li Alan[ Li, Sin-Hong ] <alan.li@linecorp.com>
2025-01-14 12:30:54 +0800

    Update copyright header

--- src/openFeature/index.ts
+++ src/openFeature/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 LY Corporation. All rights reserved.
+ * Copyright (c) 2024-2025 LY Corporation. All rights reserved.
  * LY Corporation PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 import { OpenFeature } from '../definition';
